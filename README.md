26af776afb694412dbb7cf44a543626a7334269c
Ben Lawson <blinternetgeek@gmail.com>
2018-04-23 23:46:03 -0700

    curl builds at aurora's build time

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Aurora uses cmake to build, test, and manage dependencies. To get started, run `
 
 ## Generating Documentation
 Aurora uses Doxygen to generate documentation. To generate documentation, you must have Doxygen installed. Then run `make doc`. Documentation will be generated in the `/docs` directory.
+
+## Testing
+Aurora uses googletest to create and run tests. After building all tests with `make`, you can run all tests with `make test`.
--- cmake/curl.cmake
+++ cmake/curl.cmake
@@ -1,33 +1,46 @@
-macro(fetch_curl _download_module_path _download_root)
-  set(CURL_DOWNLOAD_ROOT ${_download_root})
+cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
 
-  # copy download cmake file to the curl download root
-  configure_file(
-    ${_download_module_path}/curl-download.cmake
-    ${_download_root}/CMakeLists.txt
-    @ONLY
-    )
-  unset(CURL_DOWNLOAD_ROOT)
+include(ExternalProject)
 
-  execute_process(
-    COMMAND
-    "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
-    WORKING_DIRECTORY
-    ${_download_root}
-    )
+# the libcurl CMake file is poorly maintained
+# so we build and add the target ourself
 
-  # download & install curl
-  execute_process(
-    COMMAND
-    "${CMAKE_COMMAND}" --build .
-    WORKING_DIRECTORY
-    ${_download_root}
-    )
+set(CURL_DOWNLOAD_ROOT ${THIRD_PARTY_DIR}/curl)
+set(CURL_PREFIX ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/curl)
 
+# configure command
+# TODO: enable SSL & zlib, windows support
+set(CURL_CONFIGURE ${CURL_DOWNLOAD_ROOT}/curl-src/configure
+  --enable-static
+  --disable-shared
+  --without-ssl
+  --without-zlib
+  --disable-manual
+  --disable-ldap
+  --without-libidn2
+  --disable-cookies
+  --disable-ftp
+  --disable-imap
+  --prefix=${CURL_PREFIX})
 
-  set(CURL_LIBRARY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/curl/lib/libcurl.a)
-  add_library(libcurl STATIC IMPORTED)
-  #set_target_properties(curl PROPERTIES IMPORTED_LOCATION  ${CURL_LIBRARY})
-  set_property(TARGET libcurl PROPERTY IMPORTED_LOCATION ${CURL_LIBRARY})
+ExternalProject_Add(
+  curl
+  PREFIX ${CURL_DOWNLOAD_ROOT}
+  SOURCE_DIR ${CURL_DOWNLOAD_ROOT}/curl-src
+  BINARY_DIR ${CURL_DOWNLOAD_ROOT}/curl-build
+  GIT_REPOSITORY https://github.com/curl/curl.git
+  GIT_TAG curl-7_59_0
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ${CURL_DOWNLOAD_ROOT}/curl-src/buildconf
+  CONFIGURE_COMMAND ${CURL_CONFIGURE}
+  BUILD_COMMAND make
+  INSTALL_COMMAND make install
+  TEST_COMMAND ""
+  )
 
-endmacro()
+# add libcurl target
+set(LIBCURL_FILENAME ${CMAKE_STATIC_LIBRARY_PREFIX}curl${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(CURL_LIBRARY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/curl/lib/${LIBCURL_FILENAME})
+add_library(libcurl STATIC IMPORTED)
+set_property(TARGET libcurl PROPERTY IMPORTED_LOCATION ${CURL_LIBRARY})
+add_dependencies(libcurl curl)
