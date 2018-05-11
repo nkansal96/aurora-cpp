f42b07be873c1a628dd59a99c08d6532aa87f669
Ben Lawson <blinternetgeek@gmail.com>
2018-05-10 20:13:31 -0700

    cleaned up root CMakeLists, interpret demo

--- examples/interpret_demo.cpp
+++ examples/interpret_demo.cpp
@@ -1,40 +1,33 @@
-#include "aurora/Config.h"
-#include "aurora/Text.h"
-#include "aurora/Interpret.h"
-#include <cstdlib>
+#include <aurora/Aurora.h>
 #include <string>
 #include <iostream>
 
-void setAuroraConfiguration();  
+void setAuroraConfiguration();
 void printInterpretationIntent(const aurora::Interpret& interpretation);
 void printInterpretationEntities(const aurora::Interpret& interpretation);
 
 int main() {
-  
   setAuroraConfiguration();
-  
+
   for(;;) {
-    std::cout << "Enter a phrase to be interpretted as> ";
+    std::cout << "Enter a phrase to be interpretted > ";
     std::string phrase;
     std::getline(std::cin, phrase);
-    
+
     //create a Text object
     aurora::Text auroraText(phrase);
-      
-    
+
     try {
       //interpret Text
       aurora::Interpret interpretation = auroraText.interpret();
-      
+
       printInterpretationIntent(interpretation);
       printInterpretationEntities(interpretation);
     }
     catch (std::exception &e) {
       std::cout << "Exception occurred in interpretation: " << e.what() << std::endl;
     }
-    
   }
-  
 }
 
 void setAuroraConfiguration() {
@@ -51,7 +44,7 @@ void setAuroraConfiguration() {
     aurora::config.appID = getenv("APP_ID");
   }
 
-  //device ID is optional, if not set, set as empty string                           
+  //device ID is optional, if not set, set as empty string
   if(!getenv("DEVICE_ID")) {
     aurora::config.deviceID = "";
   } else {
@@ -60,19 +53,18 @@ void setAuroraConfiguration() {
 }
 
 void printInterpretationIntent(const aurora::Interpret& interpretation) {
-  if (interpretation.intent != "") { //check to see if intent was understood                                          
-    std::cout << "intent: " << interpretation.intent << std::endl; //print intent                
+  if (interpretation.intent != "") { //check to see if intent was understood
+    std::cout << "intent: " << interpretation.intent << std::endl; //print intent
   } else {
     std::cout << "Error, intention was unclear" << std::endl;
   }
 }
 
 void printInterpretationEntities(const aurora::Interpret& interpretation) {
-  if (!interpretation.entities.empty()){ //check to see if no information was detected                                
-    
-    //print entities                                                                                                  
-    for(std::unordered_map<std::string, std::string>::const_iterator it = interpretation.entities.begin(); it != interpretation.entities.end(); it++){
-      std::cout << it->first << " : " << it->second << std::endl;
+  if (!interpretation.entities.empty()){ //check to see if no information was detected
+    //print entities
+    for(const auto &entity : interpretation.entities){
+      std::cout << entity.first << ": " << entity.second << std::endl;
     }
   } else {
     std::cout << "Error, no information pertaining to intent" << std::endl;
